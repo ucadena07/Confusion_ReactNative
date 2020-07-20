1aef2c1ee9d1abf622a74b9b69d9392bfa4879ce
Ulises Cadena <ucadena07@gmail.com>
2020-07-20 12:57:39 -0500

    react navigation part 1

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Main from './components/MainComponent';
-
+import 'react-native-gesture-handler';
 
 export default class App extends React.Component {
   render(){
